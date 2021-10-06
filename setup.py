ab44feacc571f46b8c19bd4eb90e6257d6e83da0
guilhermedonizetti <guilhermetecnologias@gmail.com>
2021-10-06 11:37:07 -0300

    Changing the license and setup.

--- setup.py
+++ setup.py
@@ -1,34 +1,24 @@
 from setuptools import setup, version
 
 setup(
-    name = 'RoutePy',
+    name = 'WayPy',
     version = '0.1.0',
     author = 'Guilherme Donizetti and Luiz Fernando Rodrigues',
     author_email = 'guilhermetecnologias@gmail.com',
-    packages = ['routepy'],
-    description = 'Um pacote para gerar gráficos simples.',
-    long_description = 'O pacote apenas transforma duas listas de valores em um gráfico.'
-                     + 'from grafico.main import Grafico '
-                     + 'gf = Grafico() '
-                     + 'gf.generate_chart(datas_x, datas_y, type_chart, view, label_x, label_y, title) '
-                     + ' '
-                     + 'datas_x = é uma lista de valores para o eixo X.'
-                     + 'datas_y = é uma lista de valores para o eixo Y.'
-                     + 'type_chart = Opcional. Se refere ao tipo de gráfico que pode ser bar, plotou scatter. Por padrão é plot.'
-                     + 'view = Opcional. Se for True o gráfico vai abrir assimque executar, se for falso ele apenas salva a imagem. Por padrão é True.'
-                     + 'label_x = Opcional. Texto a ser exibido no eixo X.'
-                     + 'datas_y = Opcional. Texto a ser exibido no eixo Y.'
-                     + 'title = Opcional. Título do gráfico.',
-    url = 'https://github.com/guilhermedonizetti/Grafico',
+    packages = ['waypy'],
+    description = 'Package to find a path between two points in a graph.',
+    long_description = 'By entering a list of nodes and edges, the package is able to try to find a path between any two points.<br>'
+                     + 'The package contains different search methods that can get different results.',
+    url = 'https://github.com/guilhermedonizetti/WayPy',
     project_urls = {
-        'Código fonte': 'https://github.com/guilhermedonizetti/Grafico'
+        'Source code': 'https://github.com/guilhermedonizetti/WayPy'
     },
     license = 'MIT',
-    keywords = 'gerar gráfico',
+    keywords = 'way graph',
     classifiers = [
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
-        'Natural Language :: Portuguese (Brazilian)',
+        'Natural Language :: English',
         'Operating System :: OS Independent'
     ]
 )
