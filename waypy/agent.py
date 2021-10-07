8810aad89368d5094057532f92c31e999c14a365
SACRIER <caravigneh@outlook.com>
2021-10-07 10:07:02 -0300

    Starting the translation

--- waypy/agent.py
+++ waypy/agent.py
@@ -9,138 +9,139 @@ class Agente(busca):
     nodes = []
     graphs = []
 
-    #Inicializa as listas que sao de valores padrao
+    #Initialize lists that are from default values
     def __init__(self):
                 
-        #Declara o nome dos metodos a serem escolhidos para comparar com a entrada
+        #Declare the name of the methods to be chosen to compare with the input
         self.methods = ["AMPLITUDE", "PROFUNDIDADE", "PROFUNDIDADE LIMITADA",
                    "APROFUNDAMENTO ITERATIVO", "BIDIRECIONAL"]
 
-        #Declara o nome dos metodos a serem chamados para executar
+        #Declare the name of the methods to be called to execute
         self.methods_main = ["amplitude", "profundidade", "profundidade_limitada",
                         "aprofundamento_iterativo", "bidirecional"]
         
-        #Inicia a lista que vai receber as rotas
+        #Starts the list that will receive the routes
         self.route_starting =  self.route_arrival = []
 
 
     def encontrar_atendimento(self, cidade_final, metodo, limite=False):
-        """Metodo para encontrar um caminho entre a cidade que recebeu a AH e o hospital mais proximo.
-        Recebe a cidade que teve a ajuda humanitaria e realiza as tentativas de gerar
-        uma rota partindo dessa cidade ate a mais proxima dentre as da lista 'arrival_points'."""
+        """Method to find a way between the city that received the AH and the nearest hospital.
+            Receives the city that had humanitarian aid and makes attempts to generate
+            a route from this city to the closest one on the list 'arrival_points'."""
 
         tam_caminho = 100000
 
-        #se escolher Amplitude
+        #if Amplitude is chosen...
+
         if metodo == self.methods[0]:
-            for i in self.arrival_points: #para cada cidade da lista tenta um caminho...
+            for i in self.arrival_points: #for each city on the list try a path...
                 caminho = self.amplitude(cidade_final.upper(), i, self.nodes, self.graphs)
-                #o menor caminho sera o atual
+                #the shortest path will be the current
                 if len(caminho) < tam_caminho:
                     tam_caminho = len(caminho)
                     self.route_starting = caminho
             return self.route_starting
         
-        #se escolher Profundidade
+        #if Depth is chosen... 
         if metodo == self.methods[1]:
-            for i in self.arrival_points: #para cada cidade da lista tenta um caminho...
+            for i in self.arrival_points: #for each city on the list try a path...
                 caminho = self.profundidade(cidade_final.upper(), i, self.nodes, self.graphs)
-                #o menor caminho sera o atual
+                #the shortest path will be the current
                 if len(caminho) < tam_caminho:
                     tam_caminho = len(caminho)
                     self.route_starting = caminho
             return self.route_starting
         
-        #se escolher Profundidade Limitada
+        #if Limited Depth is chosen...
         if metodo == self.methods[2]:
-            for i in self.arrival_points: #para cada cidade da lista tenta um caminho...
+            for i in self.arrival_points: #for each city on the list try a path...
                 caminho = self.profundidade_limitada(cidade_final.upper(), i, 4, self.nodes, self.graphs)
-                #o menor caminho sera o atual
+                #the shortest path will be the current
                 if len(caminho) < tam_caminho:
                     tam_caminho = len(caminho)
                     self.route_starting = caminho
             return self.route_starting
         
-        #se escolher Aprofundamento iterativo
+        #if Iterative Deepening is chosen...
         if metodo == self.methods[3]:
-            for i in self.arrival_points: #para cada cidade da lista tenta um caminho...
+            for i in self.arrival_points: #for each city on the list try a path...
                 caminho = self.aprofundamento_iterativo(cidade_final.upper(), i, self.nodes, self.graphs)
-                #o menor caminho sera o atual
+                #the shortest path will be the current
                 if len(caminho) < tam_caminho:
                     tam_caminho = len(caminho)
                     self.route_starting = caminho
             return self.route_starting
         
-        #se escolher Bidirecional
+        #if Bidirectional is chosen...
         if metodo == self.methods[4]:
-            for i in self.arrival_points: #para cada cidade da lista tenta um caminho...
+            for i in self.arrival_points: #for each city on the list try a path...
                 caminho = self.bidirecional(cidade_final.upper(), i, self.nodes, self.graphs)
-                #o menor caminho sera o atual
+                #the shortest path will be the current
                 if len(caminho) < tam_caminho:
                     tam_caminho = len(caminho)
                     self.route_starting = caminho
             return self.route_starting
 
     def encontrar_ajuda_humanitaria(self, cidade_final, metodo, limite=False):
-        """Metodo para encontrar um caminho entre dois pontos.
-        Recebe a cidade que precisa da ajuda humanitaria e realiza as tentativas
-        de gerar uma rota partindo das cidades da lista 'starting_points' usando menos Memoria."""
+        """Method to find a way between two points.
+           Receives the city that needs humanitarian aid and makes the attempts
+           to generate a route starting from the cities in the 'starting_points' list using less Memory."""
         
         tam_caminho = 100000
 
-        #se escolher Amplitude
+        #if Amplitude is chosen...
         if metodo == self.methods[0]:
-            for i in self.starting_points: #para cada cidade da lista tenta um caminho...
+            for i in self.starting_points: #for each city on the list try a path...
                 caminho = self.amplitude(i, cidade_final.upper(), self.nodes, self.graphs)
-                #o menor caminho sera o atual
+                #the shortest path will be the current
                 if len(caminho) < tam_caminho:
                     tam_caminho = len(caminho)
                     self.route_starting = caminho
             return self.route_starting
         
-        #se escolher Profundidade
+        #if Depth is chosen...
         if metodo == self.methods[1]:
-            for i in self.starting_points: #para cada cidade da lista tenta um caminho...
+            for i in self.starting_points: #for each city on the list try a path...
                 caminho = self.profundidade(i, cidade_final.upper(), self.nodes, self.graphs)
-                #o menor caminho sera o atual
+                #the shortest path will be the current
                 if len(caminho) < tam_caminho:
                     tam_caminho = len(caminho)
                     self.route_starting = caminho
             return self.route_starting
         
-        #se escolher Profundidade Limitada
+        #if Limited Depth is chosen...
         if metodo == self.methods[2]:
-            for i in self.starting_points: #para cada cidade da lista tenta um caminho...
+            for i in self.starting_points: #for each city on the list try a path...
                 caminho = self.profundidade_limitada(i, cidade_final.upper(), 4, self.nodes, self.graphs)
-                #o menor caminho sera o atual
+                #the shortest path will be the current
                 if len(caminho) < tam_caminho:
                     tam_caminho = len(caminho)
                     self.route_starting = caminho
             return self.route_starting
         
-        #se escolher Aprofundamento iterativo
+        #if Iterative Deepening is chosen...
         if metodo == self.methods[3]:
-            for i in self.starting_points: #para cada cidade da lista tenta um caminho...
+            for i in self.starting_points: #for each city on the list try a path...
                 caminho = self.aprofundamento_iterativo(i, cidade_final.upper(), self.nodes, self.graphs)
-                #o menor caminho sera o atual
+                #the shortest path will be the current
                 if len(caminho) < tam_caminho:
                     tam_caminho = len(caminho)
                     self.route_starting = caminho
             return self.route_starting
         
-        #se escolher Bidirecional
+        #if Bidirectional is chosen...
         if metodo == self.methods[4]:
-            for i in self.starting_points: #para cada cidade da lista tenta um caminho...
+            for i in self.starting_points: #for each city on the list try a path...
                 caminho = self.bidirecional(i, cidade_final.upper(), self.nodes, self.graphs)
-                #o menor caminho sera o atual
+                #the shortest path will be the current
                 if len(caminho) < tam_caminho:
                     tam_caminho = len(caminho)
                     self.route_starting = caminho
             return self.route_starting
     
-    #Funcao para unir as rotas de ajuda humanitaria e atendimento
+    #Function to unite humanitarian aid and care routes
     def unifica_caminho(self, rota_AH, rota_At):
-        """Metodo para unificar o caminho, unir as rotas de AH e Atendimento"""
+        """Method to unify the path, unite the AH and Service routes"""
 
         caminho_unico = []
         for i in rota_AH:
